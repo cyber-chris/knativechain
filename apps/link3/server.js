b17fae1e87f7cffb86d017b3e25534896435b0de
utherp0 <ian.lawson@redhat.com>
2020-12-16 14:39:08 +0000

    First drop

---
+++ apps/link3/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var fs      = require('fs');
+var app     = express();
+var eps     = require('ejs');
+var got     = require('got');
+var http = require('http');
+var url = require('url');
+var ippackage = require('ip');
+var os = require('os');
+
+app.engine('html', require('ejs').renderFile);
+
+app.use( '/scripts', express.static('scripts'));
+app.use( '/styles', express.static('styles'));
+app.use( '/images', express.static('images'));
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+// Comment for git testing again
+app.get('/', function (req, res)
+{
+  console.log( "Request received, serving YELLOW....");
+  
+  res.write( "yellow");
+  res.end();
+  return;
+});
