0aad8b32da32645af3086afd2fd562885d4bbce8
utherp0 <ian.lawson@redhat.com>
2021-01-05 15:49:48 +0000

    Added a local HTML for testing the microservices. Requires you edit the HTML and add the four endpoints

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,17 +17,19 @@ oc create -f ../yaml/link1-ksvc.yaml
 oc create -f ../yaml/link2-ksvc.yaml
 oc create -f ../yaml/link3-ksvc.yaml
 oc create -f ../yaml/link4-ksvc.yaml
-kn service update link1 --revision-name=v2 --env COLOUR=blue
-kn service update link2 --revision-name=v2 --env COLOUR=yellow
-kn service update link3 --revision-name=v2 --env COLOUR=green
-kn service update link4 --revision-name=v2 --env COLOUR=red
-kn service update link1 --revision-name=v3 --env COLOUR=yellow
-kn service update link2 --revision-name=v3 --env COLOUR=blue
-kn service update link3 --revision-name=v3 --env COLOUR=red
-kn service update link4 --revision-name=v3 --env COLOUR=green
+kn service update link1 --revision-name=v2 --env COLOUR=purple
+kn service update link2 --revision-name=v2 --env COLOUR=pink
+kn service update link3 --revision-name=v2 --env COLOUR=cyan
+kn service update link4 --revision-name=v2 --env COLOUR=brown
+kn service update link1 --revision-name=v3 --env COLOUR=#009000
+kn service update link2 --revision-name=v3 --env COLOUR=#000090
+kn service update link3 --revision-name=v3 --env COLOUR=#900000
+kn service update link4 --revision-name=v3 --env COLOUR=#666666
 kn service update link1 --traffic link1-v1=40,link1-v2=30,link1-v3=30
 kn service update link2 --traffic link2-v1=30,link2-v2=40,link2-v3=30
 kn service update link3 --traffic link3-v1=30,link3-v2=30,link3-v3=40
 kn service update link4 --traffic link4-v1=30,link4-v2=30,link4-v3=40
-
-
+export LINK1URL=$(oc get ksvc/link1 -o jsonpath='{.status.url}')
+export LINK2URL=$(oc get ksvc/link2 -o jsonpath='{.status.url}')
+export LINK3URL=$(oc get ksvc/link3 -o jsonpath='{.status.url}')
+export LINK4URL=$(oc get ksvc/link1 -o jsonpath='{.status.url}')
